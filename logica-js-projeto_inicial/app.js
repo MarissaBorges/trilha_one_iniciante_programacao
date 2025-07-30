62961c582f5ec30688eaaa424be90adc9e25a6b0
Marissa Borges <marissaborges2006@gmail.com>
2025-07-30 14:55:36 -0300

    Aula 1: Iniciando com JavaScript

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+alert("Bem-vindo ao jogo do número secreto");
+let numeroSecreto = 9;
+let tentativa = prompt("Insira um valor entre 0 e 10");
+
+if (numeroSecreto == tentativa) {
+  console.log("Acertouuu");
+}
